038170e78be7b3948ee314fc8de1aa37b66d8fed
Jose Rosales <josegregorio.5019@hotmail.com>
2020-11-11 10:52:03 -0430

    Updates

---
+++ precache-manifest.7efb20b5662e51c253d1487cc2d60322.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7b00967819745889f1847085a9b8b9",
+    "url": "/brokechain/index.html"
+  },
+  {
+    "revision": "b847cf3cbccdd1ac792d",
+    "url": "/brokechain/static/css/2.26a6ae56.chunk.css"
+  },
+  {
+    "revision": "8ceaf18b71abd948b159",
+    "url": "/brokechain/static/css/main.604cac0a.chunk.css"
+  },
+  {
+    "revision": "b847cf3cbccdd1ac792d",
+    "url": "/brokechain/static/js/2.74dea960.chunk.js"
+  },
+  {
+    "revision": "6ac571e44e61ac66ab0ab3608d92d99b",
+    "url": "/brokechain/static/js/2.74dea960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ceaf18b71abd948b159",
+    "url": "/brokechain/static/js/main.c5715344.chunk.js"
+  },
+  {
+    "revision": "f06026264dc00ed264c6",
+    "url": "/brokechain/static/js/runtime-main.5607c649.js"
+  },
+  {
+    "revision": "ad4c6210ae48ce9af5a80684d70d84a8",
+    "url": "/brokechain/static/media/banescop.ad4c6210.png"
+  },
+  {
+    "revision": "8a0984bbf8b3902366ca1839303e483a",
+    "url": "/brokechain/static/media/bodp.8a0984bb.png"
+  },
+  {
+    "revision": "cd804b0ef013330f580b4e1d25ba4540",
+    "url": "/brokechain/static/media/foto.cd804b0e.png"
+  },
+  {
+    "revision": "3111f01b2fd440ab787e86df58d4edf2",
+    "url": "/brokechain/static/media/mercantilp.3111f01b.png"
+  }
+]);
